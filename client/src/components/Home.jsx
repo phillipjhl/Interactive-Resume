0dd70e366fe0f16e4c647c1b8337451a912ef0ab
Phillip Langland <philliplangland@gmail.com>
2018-10-15 23:50:30 -0500

    Styling with breakpoints

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,33 +9,43 @@ class HomePage extends Component {
     render() {
         return (
             <Fragment>
+
                 <section className="home">
+
                     <div className="overlay opacity3"></div>
+
                     <div className="container">
+
                         <div className="name-block">
                             <div className="name-block-container">
                                 <h1 className="text-primary"><span>Hi, I'm</span>Phillip Langland</h1>
                             </div>
                         </div>
+
                         <div className="menu-blocks">
+
                             <div className="about-block menu-block">
                                 <div className="about-block-container">
-                                    <h1 className="text-primary">Resume</h1>
+                                    <h2 className="about menu-item text-primary">Resume</h2>
                                 </div>
                             </div>
+
                             <div className="portfolio-block menu-block">
                                 <div className="portfolio-block-container">
-                                    <h1 className="text-primary">Portfolio</h1>
+                                    <h2 className="portfolio menu-item text-primary">Portfolio</h2>
                                 </div>
                             </div>
+
                             <div className="contact-block menu-block">
                                 <div className="contact-block-container">
-                                    <h1 className="text-primary">Contact</h1>
+                                    <h2 className="contact menu-item text-primary">Contact</h2>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </section>
+
             </Fragment>
         );
     }
