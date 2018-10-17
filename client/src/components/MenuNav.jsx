49efcec95f70f2e933878ec0a1a599ae33117b84
Phillip Langland <philliplangland@gmail.com>
2018-10-16 21:30:08 -0500

    added functioning menu navigator for other pages

---
+++ client/src/components/MenuNav.jsx
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NavBar from './NavBar';
+import Resume from './Resume';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+
+export default class MenuNav extends Component {
+
+    render() {
+        return (
+            <Router>
+                <Fragment>
+                    <NavBar />
+                    <Switch>
+                        <Route path="/menu/resume" component={Resume} />
+                        <Route path="/menu/portfolio" component={Portfolio} />
+                        <Route path="/menu/contact" component={Contact} />
+                    </Switch>
+                </Fragment>
+            </Router>
+        );
+    }
+
+}
