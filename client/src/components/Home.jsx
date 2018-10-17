5d8099092e9ffe63e85d980130895bdcbcfbbf59
Phillip Langland <philliplangland@gmail.com>
2018-10-16 20:04:29 -0500

    component nesting

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,8 @@
 import React, { Component, Fragment } from 'react';
-import { Link } from 'react-router-dom';
 import bg from '../docs/light-bg.jpg';
 
 import NameBlock from './NameBlock';
+import MenuBlocks from './MenuBlocks';
 
 class HomePage extends Component {
 
@@ -24,29 +24,8 @@ class HomePage extends Component {
 
                         <NameBlock />
 
-                        <div className="menu-blocks">
+                        <MenuBlocks />
 
-                            <div className="about-block menu-block bg-compliment">
-                                <div className="about-block-container">
-                                    <h2 className="about menu-item text-primary">Resume</h2>
-                                </div>
-                            </div>
-
-                            <div className="portfolio-block menu-block bg-contrast">
-                                <div className="portfolio-block-container">
-                                    <h2 className="portfolio menu-item text-secondary">Portfolio</h2>
-                                </div>
-                            </div>
-
-                            <Link to='/contact'>
-                                <div className="contact-block menu-block bg-primary">
-                                    <div className="contact-block-container">
-                                        <h2 className="contact menu-item text-secondary">Contact</h2>
-                                    </div>
-                                </div>
-                            </Link>
-
-                        </div>
                     </div>
                 </section>
 
---
+++ client/src/components/MenuBlocks.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class MenuBlocks extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+
+            <div className="menu-blocks">
+
+                <div className="about-block menu-block bg-compliment">
+                    <div className="about-block-container">
+                        <h2 className="about menu-item text-primary">Resume</h2>
+                    </div>
+                </div>
+
+                <div className="portfolio-block menu-block bg-contrast">
+                    <div className="portfolio-block-container">
+                        <h2 className="portfolio menu-item text-secondary">Portfolio</h2>
+                    </div>
+                </div>
+
+                <Link to='/contact'>
+                    <div className="contact-block menu-block bg-primary">
+                        <div className="contact-block-container">
+                            <h2 className="contact menu-item text-secondary">Contact</h2>
+                        </div>
+                    </div>
+                </Link>
+
+            </div>
+
+        );
+    }
+}
