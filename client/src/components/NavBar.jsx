a0d0e2af1a92d00324dbed5300549a28422edb56
Phillip Langland <philliplangland@gmail.com>
2018-10-17 22:38:21 -0500

    minor changes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 function NavBar(props) {
     return (
         <nav className="navbar navbar-expand-lg bg-secondary text-primary" >
-            <Link to='/' className="navbar-brand" >Home</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="collapse navbar-collapse" id="navbar">
                 <div className="navbar-nav flex-row-reverse">
-                    <Link to="/contact" className="nav-item nav-link">Contact</Link>
-                    <Link to="/" className="nav-item nav-link" >Home <span className="sr-only">(current)</span></Link>
+                    <Link to="/menu/resume" className="nav-item nav-link text-primary">Resume</Link>
+                    <Link to="/menu/portfolio" className="nav-item nav-link text-primary">Portfolio</Link>
+                    <Link to="/menu/contact" className="nav-item nav-link text-primary">Contact</Link>
                 </div>
             </div>
         </nav>
